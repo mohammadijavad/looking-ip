26ec8a078a3b71f4c85066aa519df19f9d2d30d2
javad <javadmohammadi.js@gmail.com>
2024-10-12 00:51:06 +0330

    fix: timeout navigate login page

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,6 @@
 "use client";
 import Link from "next/link";
 import { Box, Button } from "@mui/material";
-import styled from "styled-components";
-
-const StyledLink = styled(Link)`
-  text-decoration: none;
-  color: inherit;
-  padding: 1rem;
-  font-size: 1rem;
-`;
 
 export default function Home() {
   return (
@@ -18,9 +10,9 @@ export default function Home() {
       alignItems="center"
       minHeight="100vh"
     >
-      <Button variant="contained">
-        <StyledLink href="/login">Login 🔐</StyledLink>
-      </Button>
+      <Link href="/login" passHref>
+        <Button variant="contained">Login 🔐</Button>
+      </Link>
     </Box>
   );
 }
